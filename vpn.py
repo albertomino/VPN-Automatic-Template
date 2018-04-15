8b1104765e4cba9e3e45fa9bec79dda4352eeaa5
Alberto Miño <clusterbyte@gmail.com>
2018-04-15 03:02:27 -0300

    se agrega a  vpn.py

--- vpn.py
+++ vpn.py
@@ -7,11 +7,11 @@ class vpn:
     def __init__(self, name, key, peer, ti, remote_enc_domain, source_enc_domain, local_sources, dports, local_server, lports, vpn_settings):
     #def __init__(self, settings):
         #self.vpn_settings = json.loads(vpn_settings)
-        self.vpn = vpn_settings["vpn"]
+        self.vpn_general = vpn_settings["vpn"]
         self.ike_proposal = vpn_settings["ike_proposal"]
         self.ike_policy = vpn_settings["ike_policy"]
         self.ike_gateway = vpn_settings["ike_gateway"]
-        self.ike_gateway =vpn_settings["ike_gateway"]
+        self.ike_gateway = vpn_settings["ike_gateway"]
         self.ipsec_proposal = vpn_settings["ipsec_proposal"]
         self.ipsec_policy = vpn_settings["ipsec_policy"]
         self.vpn_tunnel_definition = vpn_settings["vpn_tunnel_definition"]
@@ -34,63 +34,65 @@ class vpn:
 
 #PHASE-1
     def phase_1(self):
-        ike = '' +\
-        '\nset security ike proposal %s description %s' % (self.ike_proposal["name"], self.vpn["description"]) +\
-        '\nset security ike proposal %s authentication-method %s' % (self.ike_proposal["name"], self.ike_proposal["authentication_method"]) +\
-        '\nset security ike proposal %s dh-group %s' % (self.ike_proposal["name"], self.ike_proposal["diffie_hellman_group"]) +\
-        '\nset security ike proposal %s authentication-algorithm %s' % (self.ike_proposal["name"], self.ike_proposal["authentication_algorithm"]) +\
-        '\nset security ike proposal %s encryption-algorithm %s' % (self.ike_proposal["name"], self.ike_proposal["encryption_algorithm"]) +\
-        '\nset security ike proposal %s lifetime-seconds %s' % (self.ike_proposal["name"], self.ike_proposal["lifetime"]) +\
-        '\n' +\
-        '\nset security ike policy %s mode main' % self.ike_policy["name"] +\
-        '\nset security ike policy %s proposals %s' % (self.ike_policy["name"], self.ike_proposal["name"]) +\
-        '\nset security ike policy %s pre-shared-key ascii-text %s' % (self.ike_policy["name"], self.ike_policy["pre_shared_key"])
+        ike = "" +\
+        "\nset security ike proposal %s description %s" % (self.ike_proposal["name"], self.vpn_general["description"]) +\
+        "\nset security ike proposal %s authentication-method %s" % (self.ike_proposal["name"], self.ike_proposal["authentication_method"]) +\
+        "\nset security ike proposal %s dh-group %s" % (self.ike_proposal["name"], self.ike_proposal["diffie_hellman_group"]) +\
+        "\nset security ike proposal %s authentication-algorithm %s" % (self.ike_proposal["name"], self.ike_proposal["authentication_algorithm"]) +\
+        "\nset security ike proposal %s encryption-algorithm %s" % (self.ike_proposal["name"], self.ike_proposal["encryption_algorithm"]) +\
+        "\nset security ike proposal %s lifetime-seconds %s" % (self.ike_proposal["name"], self.ike_proposal["lifetime"]) +\
+        "\n" +\
+        "\nset security ike policy %s mode main" % self.ike_policy["name"] +\
+        "\nset security ike policy %s proposals %s" % (self.ike_policy["name"], self.ike_proposal["name"]) +\
+        "\nset security ike policy %s pre-shared-key ascii-text %s" % (self.ike_policy["name"], self.ike_policy["pre_shared_key"])
 
         return ike
 
 #PHASE-2
     def phase_2(self):
-        ipsec = '' +\
-        '\nset security ipsec proposal %s protocol esp' % self.vpn_name +\
-        '\nset security ipsec proposal %s authentication-algorithm hmac-sha1-96' % self.vpn_name +\
-        '\nset security ipsec proposal %s encryption-algorithm aes-256-cbc' % self.vpn_name +\
-        '\nset security ipsec proposal %s lifetime-seconds 3600' % self.vpn_name +\
-        '\n' +\
-        '\nset security ipsec policy %s_IPSEC_POLICY proposals %s' % (self.vpn_name, self.vpn_name)
+        ipsec = "" +\
+        "\nset security ipsec proposal %s protocol esp" % self.ipsec_proposal["name"] +\
+        "\nset security ipsec proposal %s authentication-algorithm %s" % (self.ipsec_proposal["name"], self.ipsec_proposal["authentication_algorithm"]) +\
+        "\nset security ipsec proposal %s encryption-algorithm %s" % (self.ipsec_proposal["name"], self.ipsec_proposal["encryption_algorithm"]) +\
+        "\nset security ipsec proposal %s lifetime-seconds %s" % (self.ipsec_proposal["name"], self.ipsec_proposal["lifetime"]) +\
+        "\n" +\
+        "\nset security ipsec policy %s proposals %s" % (self.ipsec_policy["name"], self.ipsec_proposal["name"])
+
+        if self.ipsec_policy["pfs"] == True: ipsec = ipsec + "\nset security ipsec policy %s perfect-forward-secrecy keys %s" % (self.ipsec_policy["name"], self.ipsec_policy["keys"])
 
         return ipsec
 
 
 #IKE GATEWAY
 
-    def ike_gateway(self):
-        ikegateway = '' +\
-        '\nset security ike gateway %s ike-policy %s' % (self.vpn_name, self.vpn_name) +\
-        '\nset security ike gateway %s address %s' % (self.vpn_name, self.peer) +\
-        '\nset security ike gateway %s dead-peer-detection always-send' % self.vpn_name +\
-        '\nset security ike gateway %s external-interface reth1.404' % self.vpn_name +\
-        '\nset security ike gateway %s general-ikeid' % self.vpn_name
+    def gateway(self):
+        ikegateway = "" +\
+        "\nset security ike gateway %s ike-policy %s" % (self.ike_gateway["name"], self.ike_policy["name"]) +\
+        "\nset security ike gateway %s address %s" % (self.ike_gateway["name"], self.ike_gateway["address"]) +\
+        "\nset security ike gateway %s external-interface %s" % (self.ike_gateway["name"], self.ike_gateway["external_interface"]) +\
+        "\nset security ike gateway %s version %s" % (self.ike_gateway["name"], self.ike_gateway["version"])
+
+        if self.ike_gateway["general-ikeid"] == True: ikegateway = ikegateway + "\nset security ike gateway %s general-ikeid" % self.ike_gateway["name"]
 
         return ikegateway
 
 
 #VPN TUNEL DEFINITION
     def vpn(self):
-        vpntunnel = '' +\
-        '\nset security ipsec vpn %s bind-interface st0.%s' % (self.vpn_name, self.si) +\
-        '\nset security ipsec vpn %s ike gateway %s' % (self.vpn_name, self.si) +\
-        '\nset security ipsec vpn %s ike ipsec-policy %s' % (self.vpn_name, self.ipsec_policy_name) +\
-        '\nset security ipsec vpn %s establish-tunnels immediately' % self.vpn_name
+        vpntunnel = "" +\
+        "\nset security ipsec vpn %s bind-interface st0.%s" % (self.vpn_tunnel_definition["name"], self.vpn_tunnel_definition["secure_interface"]) +\
+        "\nset security ipsec vpn %s ike gateway %s" % (self.vpn_tunnel_definition["name"], self.ike_gateway["name"]) +\
+        "\nset security ipsec vpn %s ike ipsec-policy %s" % (self.vpn_tunnel_definition["name"], self.ipsec_policy["name"]) +\
+        "\nset security ipsec vpn %s establish-tunnels immediately" % self.vpn_tunnel_definition["name"]
 
         return vpntunnel
 
 
 #INTERFACE TUNEL DEFINITION
     def tunel_interface(self):
-        ti = '' +\
-        '\nset interfaces st0 unit %s description \"VPN %s\"' % (self.si, self.vpn_name) +\
-        '\nset interfaces st0 unit %s family inet mtu 1436' % self.si +\
-        '\nset security zones security-zone DMZ_VPN interfaces st0.%s' % self.si
+        ti = "" +\
+        "\nset interfaces st0 unit %s description %s" % (self.vpn_tunnel_definition["secure_interface"], self.vpn_general["description"]) +\
+        "\nset security zones security-zone DMZ_VPN interfaces st0.%s" % self.vpn_tunnel_definition["secure_interface"]
 
         return ti
 
@@ -98,16 +100,16 @@ class vpn:
 #STATIC ROUTE
     def static_route(self):
         comando = ""
-        for ip in self.remote_enc_domains:
-            comando = comando + "\n" + 'set routing-options static route %s next-hop st0.%s' % (ip.get("PROD") or ip.get("DESA"),
-            self.si)
+        for ip in self.encryption_domains["remote"]:
+            comando = comando + "\n" + "set routing-options static route %s next-hop st0.%s" % (ip.get("PROD") or ip.get("DESA"),
+            self.vpn_tunnel_definition["secure_interface"])
         return comando
 
 
 #MODIFYING PREFIX-LIST TO PROCESS THE NEW VPN TRAFFIC
     def prefix_list(self):
         comando = ""
-        for ip in self.remote_enc_domains: comando = comando + "\n" + 'set policy-options prefix-list PBR_Inet-0 %s' % (ip.get("PROD") or ip.get("DESA"))
+        for ip in self.encryption_domains["remote"]: comando = comando + "\n" + "set policy-options prefix-list PBR_Inet-0 %s" % (ip.get("PROD") or ip.get("DESA"))
         return comando
 
 
@@ -116,56 +118,59 @@ class vpn:
 #OUTBOUND TRAFFIC FROM MELI TO REMOTE-ENDPOINTS:
 
     def source_pool(self):
-        spools = '\nset security nat source pool SOURCE-POOL-%s_%s address %s' % (self.vpn_name, self.source_enc_domain, self.source_enc_domain)
-        self.snat_pool_name = 'SOURCE-POOL-%s_%s' % (self.vpn_name, self.source_enc_domain)
-        return spools
+        snat_pools = ""
+        for ip in self.nat_encryption_domains["local"]:
+            snat_pools = snat_pools + "\nset security nat source pool %s_%s_%s address %s" % (self.vpn_general["name"], ip.items(), ip.get("PROD") or ip.get("DESA"), \
+            ip.get("PROD") or ip.get("DESA"))
+        #self.snat_pool_name = "SOURCE-POOL-%s_%s" % (self.vpn_name, self.source_enc_domain)
+        return snat_pools
 
     def outbound_nat(self):
         lsources = []
         rendpoints = []
         self.dports = []
-        for ip in self.local_sources: lsources.append('set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match source-address %s' % (self.vpn_name, ipaddress.ip_network(ip)))
-        #for ip in self.remote_enc_domain: rendpoints.append('set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match #destination-address %s' % (self.vpn_name, ipaddress.ip_network(ip)))
-        for port in self.dports: dports.append('set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match destination-port %s' % (self.vpn_name, port))
-        outbound_nat = ''
-        for sources in lsources: outbound_nat = outbound_nat + '\n' + sources
-        #for rendpoint in rendpoints: outbound_nat = outbound_nat + '\n' + rendpoint
-        outbound_nat = outbound_nat + '\nset security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match destination-address %s' % (self.vpn_name, self.remote_enc_domain)
-        for dport in self.dports: outbound_nat = outbound_nat + '\n' + dport
-        outbound_nat = outbound_nat + '\n' + 'set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s then source-nat pool %s \n' % (self.vpn_name, self.snat_pool_name)
+        for ip in self.local_sources: lsources.append("set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match source-address %s" % (self.vpn_name, ipaddress.ip_network(ip)))
+        #for ip in self.remote_enc_domain: rendpoints.append("set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match #destination-address %s" % (self.vpn_name, ipaddress.ip_network(ip)))
+        for port in self.dports: dports.append("set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match destination-port %s" % (self.vpn_name, port))
+        outbound_nat = ""
+        for sources in lsources: outbound_nat = outbound_nat + "\n" + sources
+        #for rendpoint in rendpoints: outbound_nat = outbound_nat + "\n" + rendpoint
+        outbound_nat = outbound_nat + "\nset security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s match destination-address %s" % (self.vpn_name, self.remote_enc_domain)
+        for dport in self.dports: outbound_nat = outbound_nat + "\n" + dport
+        outbound_nat = outbound_nat + "\n" + "set security nat source rule-set NAT_SRC_VPN rule SNAT-VPN-%s then source-nat pool %s \n" % (self.vpn_name, self.snat_pool_name)
         return outbound_nat
 
     def destination_pool(self):
-        dpool = 'set security nat destination pool DNAT-POOL-%s_%s address %s' % (self.vpn_name, self.local_server, self.local_server)
-        self.dnat_pool_name = 'DNAT-POOL-%s_%s' % (self.vpn_name, self.local_server)
+        dpool = "set security nat destination pool DNAT-POOL-%s_%s address %s" % (self.vpn_name, self.local_server, self.local_server)
+        self.dnat_pool_name = "DNAT-POOL-%s_%s" % (self.vpn_name, self.local_server)
         return dpool
 
     def destination_nat(self):
         lports = []
-        for port in self.lports: lports.append('set security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s match destination-port %s' % (self.vpn_name, port))
-        destination_nat = '\nset security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s match source-address %s' % (self.vpn_name, self.remote_enc_domain) +\
-        '\nset security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s match destination-address %s' % (self.vpn_name, self.source_enc_domain)
-        for lp in lports: destination_nat = destination_nat + '\n' + lp
-        destination_nat = destination_nat + '\nset security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s then destination-nat pool %s' % (self.vpn_name, self.dnat_pool_name)
+        for port in self.lports: lports.append("set security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s match destination-port %s" % (self.vpn_name, port))
+        destination_nat = "\nset security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s match source-address %s" % (self.vpn_name, self.remote_enc_domain) +\
+        "\nset security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s match destination-address %s" % (self.vpn_name, self.source_enc_domain)
+        for lp in lports: destination_nat = destination_nat + "\n" + lp
+        destination_nat = destination_nat + "\nset security nat destination rule-set NAT_DEST_VPN rule DNAT-VPN-%s then destination-nat pool %s" % (self.vpn_name, self.dnat_pool_name)
         return destination_nat
 
 
 #SOURCE NAT FOR INBOUND TRAFFIC
 
     def inbound_source_pool(self):
-        inbound_spools = '\nset security nat source pool SOURCE-POOL-%s_%s address %s' % (self.vpn_name, self.source_enc_domain, self.source_enc_domain)
-        self.inbound_snat_pool_name = 'SOURCE-POOL-%s_%s' % (self.vpn_name, self.source_enc_domain)
+        inbound_spools = "\nset security nat source pool SOURCE-POOL-%s_%s address %s" % (self.vpn_name, self.source_enc_domain, self.source_enc_domain)
+        self.inbound_snat_pool_name = "SOURCE-POOL-%s_%s" % (self.vpn_name, self.source_enc_domain)
         return inbound_spools
 
     def inbound_nat(self):
-        inbound_nat = '\nset security nat source rule-set SNAT_VPN_TO_B2B rule SNAT_%s match source-address %s' % (self.vpn_name, self.remote_enc_domain) +\
-        '\nset security nat source rule-set SNAT_VPN_TO_B2B rule SNAT_%s match destination-address %s' % (self.vpn_name, self.local_server)
+        inbound_nat = "\nset security nat source rule-set SNAT_VPN_TO_B2B rule SNAT_%s match source-address %s" % (self.vpn_name, self.remote_enc_domain) +\
+        "\nset security nat source rule-set SNAT_VPN_TO_B2B rule SNAT_%s match destination-address %s" % (self.vpn_name, self.local_server)
         local_ports = []
-        for port in self.lports: local_ports.append('set security nat source rule-set SNAT_VPN_TO_B2B rule SNAT_%s match destination-port %s' % (self.vpn_name, port))
-        for sport in local_ports: inbound_nat = inbound_nat + '\n' + sport
-        inbound_nat = inbound_nat + '\n' + 'set security nat source rule-set SNAT_VPN_TO_B2B rule SNAT-%s then source-nat pool %s \n' % (self.vpn_name, self.inbound_snat_pool_name)
+        for port in self.lports: local_ports.append("set security nat source rule-set SNAT_VPN_TO_B2B rule SNAT_%s match destination-port %s" % (self.vpn_name, port))
+        for sport in local_ports: inbound_nat = inbound_nat + "\n" + sport
+        inbound_nat = inbound_nat + "\n" + "set security nat source rule-set SNAT_VPN_TO_B2B rule SNAT-%s then source-nat pool %s \n" % (self.vpn_name, self.inbound_snat_pool_name)
         return inbound_nat
-'''
+"""
 
 POLICIES
 
@@ -206,7 +211,7 @@ set security policies from-zone DMZ_VPN to-zone DMZ_B2B policy ACCESS_FROM_CLIEN
 
 Pre-Shared-key
 yY4bAGPGuZ67XRHz
-'''
+"""
 
 def main():
     try:
@@ -216,20 +221,20 @@ def main():
         print("config.json doesn't exist")
         return
 
-    new_vpn = vpn(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7].split(','), sys.argv[8].split(','), sys.argv[9], sys.argv[10].split(','), vpn_config)
+    new_vpn = vpn(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7].split(","), sys.argv[8].split(","), sys.argv[9], sys.argv[10].split(","), vpn_config)
     print(new_vpn.phase_1())
-    #print(new_vpn.phase_2())
-    #print(new_vpn.ike_gateway())
-    #print(new_vpn.vpn())
-    #print(new_vpn.tunel_interface())
-    #print(new_vpn.static_route())
-    #print(new_vpn.prefix_list())
-    #print(new_vpn.source_pool())
+    print(new_vpn.phase_2())
+    print(new_vpn.gateway())
+    print(new_vpn.vpn())
+    print(new_vpn.tunel_interface())
+    print(new_vpn.static_route())
+    print(new_vpn.prefix_list())
+    print(new_vpn.source_pool())
     #print(new_vpn.outbound_nat())
     #print(new_vpn.destination_pool())
     #print(new_vpn.destination_nat())
     #print(new_vpn.inbound_source_pool())
     #print(new_vpn.inbound_nat())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
